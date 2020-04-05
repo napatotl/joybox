8c8e03ed9d068879c42a0d3fe187d6bef937e655
Pichet <peemzas@gmail.com>
2020-04-05 14:43:32 +0700

    update

--- src/page/Player.js
+++ src/page/Player.js
@@ -79,7 +79,6 @@ const Player = () => {
     answers
   } = gameObj.val()
   const players = playersObj.val()
-  const isAnswerer = player && player.answererMatchIndexes ? player.answererMatchIndexes.includes(currentMatch): false
   if (loading) {
     return <div>LOADING . . .</div>
   }
@@ -97,6 +96,7 @@ const Player = () => {
       )
     }
     case 'ANSWER': {
+      const isAnswerer = player.answererMatchIndexes.includes(currentMatch)
       return (
         <Answer
           timer={timer}
@@ -110,6 +110,7 @@ const Player = () => {
       )
     }
     case 'VOTE': {
+      const isAnswerer = player.answererMatchIndexes.includes(currentMatch)
       return (
         <Vote
           timer={timer}
